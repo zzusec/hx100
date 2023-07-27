2ba91a2c44415231ba539bb3a5f65f6f645d0ec6
hx10 <55314093+zzusec@users.noreply.github.com>
2023-07-27 17:57:51 +0800

    Update app_edtunnel.js

--- app_edtunnel.js
+++ app_edtunnel.js
@@ -4,7 +4,7 @@ const net = require('net');
 
 // 创建 WebSocket 服务器
 const wss = new WebSocket.Server({ port: 443 });
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = 'c4ddda18-d5b1-4e68-9e20-b25d7a6c8304';
 let proxyIP = "64.68.192." + Math.floor(Math.random() * 255);
 
 let address = '';
